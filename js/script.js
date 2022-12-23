02b28726bbb0f3835ea2cd640f87a6e2c9e4a545
Samir <spunlae.munlae111@gmail.com>
2022-12-23 21:31:14 +0300

    First console app

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 "use strict";
 
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-console.log(4 + 4 == '8');
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+let movieName = prompt('Один из последних просмотренных фильмов?', '');
+let movieGrade = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[movieName] = movieGrade;
+
+console.log(personalMovieDB);
