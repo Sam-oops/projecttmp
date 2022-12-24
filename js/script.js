5693d9dd71a293c502ef7face2f807c9b3ac6fdc
Samir <spunlae.munlae111@gmail.com>
2022-12-24 04:40:26 +0300

    using cycle for and condition if in first app

--- js/script.js
+++ js/script.js
@@ -1,22 +1,42 @@
 "use strict";
 
-let num = 50;
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// while (num <= 55) {
-//     console.log(num);
-//     num++;
-// }
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
 
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while( num < 55);
 
-for (let i = 1; i < 8; i++) {
-    if (i === 6) {
-        // break;
-        continue;
+for (let i = 0; i < 2; i++) {
+    const movieName = prompt('Один из последних просмотренных фильмов?', '');
+    const movieGrade = prompt('На сколько оцените его?', '');
+
+    if (movieName != null && movieGrade != null && movieName != '' && movieGrade != '' && movieName.length < 50) {
+        personalMovieDB.movies[movieName] = movieGrade;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
     }
-    console.log(i);
 }
+
+    // для того чтобы вернуть пользователя к вопросам вверху
+    // нужно не заново их писать а работать с счетчиком
+    // и записать дикремент если начало работать else
+
+
+if (personalMovieDB.count < 10){
+    console.log('Просмотрено мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30){
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
+
+console.log(personalMovieDB);
