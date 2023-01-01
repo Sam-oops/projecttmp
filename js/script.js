dcf0b52968b9266a9fd078a317dc6d36dfbd5cf5
Samir <spunlae.munlae111@gmail.com>
2023-01-01 20:38:30 +0300

    Function (declaration, expression, стрелочные)

--- js/script.js
+++ js/script.js
@@ -1,42 +1,17 @@
 "use strict";
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+function ret() {
+    let num = 50;
+    return num;
+}
 
-let personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
+let anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function() {
+    console.log('hello');
 };
 
+logger();
 
-for (let i = 0; i < 2; i++) {
-    const movieName = prompt('Один из последних просмотренных фильмов?', '');
-    const movieGrade = prompt('На сколько оцените его?', '');
-
-    if (movieName != null && movieGrade != null && movieName != '' && movieGrade != '' && movieName.length < 50) {
-        personalMovieDB.movies[movieName] = movieGrade;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
-    }
-}
-
-    // для того чтобы вернуть пользователя к вопросам вверху
-    // нужно не заново их писать а работать с счетчиком
-    // и записать дикремент если начало работать else
-
-
-if (personalMovieDB.count < 10){
-    console.log('Просмотрено мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
-    console.log('Вы классический зритель');
-} else if (personalMovieDB.count >= 30){
-    console.log('Вы киноман');
-} else {
-    console.log('Произошла ошибка');
-}
-
-console.log(personalMovieDB);
+const calc = (a, b) => a + b;
